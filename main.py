cbdd7de8965e848585ef44b328eb5468995500b5
asxlvm <81298611+asxlvm@users.noreply.github.com>
2021-12-04 22:48:16 +0000

    add: functionality or whatever

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import threading, random, string
+import threading, random, string, psutil
 
 URL = "https://joinmyquiz.com"
 JOINXPATH = "//input[@placeholder='Enter a join code']"
 NAMEXPATH = "//input[@placeholder='Enter your name']"
 STARTXPATH = "//button[@class='primary-button start-game']"
 
-CODE = ""
-NAME = ""
-NUMTHREADS = 0
-REQPERTHREAD = 0
+CODE = "7441017"
+NAME = "L ratio"
+NUMTHREADS = 2
+REQPERTHREAD = 15
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-
+chrome_options.add_argument('--disable-logging')
+chrome_options.add_argument('--disable-extensions')
 #chrome_options.add_argument('--headless')
 # driver = webdriver.Chrome(options=chrome_options)
 # driver.get('https://joinmyquiz.com')
@@ -47,14 +48,20 @@ def gen_code():
     return "".join([str(random.choice(string.digits * 2 + string.ascii_lowercase)) for _ in range(7)]) 
 
 class BotInstance:
-    def __init__(self, num, code, name, driver):
+    def __init__(self, num, code, name, driver, torun):
         self.num = num
         self.code = code
-        self.name = name + gen_code()
+        self.base_name = name
+        self.name = name + f" - {gen_code()}"
         self.driver = driver
+        self.to_run = torun
+        self.ran = 0
 
     def error(self, where) -> str:
         return f"Bot {self.num} ({self.name}) failed at {where}: "
+
+    def status(self, where) -> str:
+        return f"Bot {self.num} ({self.name}) finished {where}: "
 
     def join(self) -> bool:
         try:
@@ -63,6 +70,7 @@ class BotInstance:
             inputbar = WebDriverWait(self.driver, 10, 1).until(EC.presence_of_element_located((By.XPATH, JOINXPATH)))
             inputbar.send_keys(self.code)
             inputbar.send_keys(Keys.RETURN)
+            print(f"{self.status('joining')}")
 
             return True
         except Exception as e:
@@ -75,6 +83,7 @@ class BotInstance:
             inputbar.send_keys(Keys.CONTROL + "A")
             inputbar.send_keys(self.name)
             inputbar.send_keys(Keys.RETURN)
+            print(f"{self.status('starting')}")
 
             return True
         except Exception as e:
@@ -82,8 +91,13 @@ class BotInstance:
             return False
 
     def do_all(self):
-        if self.join() is True:
-            self.start()
+        for _ in range(self.to_run):
+            if self.join() is True:
+                self.start()
+            self.ran += 1
+            self.name = self.base_name + f" - {gen_code()}"
+        self.quit()
+            
 
     def quit(self) -> None:
         try:
@@ -111,14 +125,17 @@ def main():
         REQPERTHREAD = int(input("How many times should a bot join through the same thread? (Choose this based on how much bots you want to have, if you have 10 threads and select 10 requests per thread, then the final count of bots will be 100."))
 
     for i in range(NUMTHREADS):
-        bots.append(BotInstance(i, CODE, NAME, webdriver.Chrome(options = chrome_options)))
+        #d = webdriver.Firefox(executable_path="geckodriver")
+        d = webdriver.Chrome(options = chrome_options)
+        #d.set_window_size(1120, 550)
+        bots.append(BotInstance(i, CODE, NAME, d, REQPERTHREAD))
 
     for b in bots:
         threads.append(threading.Thread(target=b.do_all))
 
     for t in threads:
-        for _ in range(REQPERTHREAD):
-            t.start()
+        t.start()
 
 if __name__ == "__main__":
     main()
+
